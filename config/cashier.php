05c53300384ebd9232dc4542f8e6dc8f9b601c43
Amr Ahmed <aemaddin@gmail.com>
2020-03-17 16:46:59 +0200

    added customer migration composer updated

--- config/cashier.php
+++ config/cashier.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * cashier package config file
- */
+
 return [
-    // Place your package's config settings here.
+
+    /*
+    |--------------------------------------------------------------------------
+    | TAP Keys
+    |--------------------------------------------------------------------------
+    |
+    | The TAP publishable key and secret key give you access to TAP's
+    | API. The "publishable" key is typically used when interacting with
+    | TAP.js while the "secret" key accesses private API endpoints.
+    |
+    */
+
+    'key' => env('TAP_KEY'),
 ];
