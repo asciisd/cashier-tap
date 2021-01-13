4d5f95dffe6c8974406b4d720acbe07d3dd53a8f
aemaddin <aemaddin@gmail.com>
2021-01-13 21:53:34 +0200

    add ability to create tap invoice

--- src/Concerns/ManagesInvoices.php
+++ src/Concerns/ManagesInvoices.php
@@ -9,8 +9,36 @@ use Tap\Invoice;
 
 trait ManagesInvoices
 {
-    public static function tapInvoices()
+    public static function tapInvoices($limit = 25)
     {
-        Invoice::all(['limit' => 25], Cashier::tapOptions());
+        Invoice::all(['limit' => $limit], Cashier::tapOptions());
+    }
+
+    public function createTapInvoice($amount, $trading_account, $currency = 'KWD', $options = [])
+    {
+        Invoice::create(array_merge([
+            "due"      => now()->getPreciseTimestamp(3),
+            "expiry"   => now()->addDays(10)->getPreciseTimestamp(3),
+            "customer" => [
+                "id" => $this->tap_id,
+            ],
+            "order"    => [
+                "amount"   => $amount,
+                "currency" => $currency,
+                "items"    => [
+                    [
+                        "amount"      => $amount,
+                        "currency"    => $currency,
+                        "description" => "Deposit on account #"
+                                         . $trading_account,
+                        "name"        => "Deposit",
+                        "quantity"    => 1,
+                    ],
+                ],
+            ],
+            "post"     => ['url' => url('/tap/webhook')],
+            "redirect" => ['url' => url(config('cashier.redirect_url'))],
+
+        ], $options), Cashier::tapOptions());
     }
 }
