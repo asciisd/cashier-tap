73e31f208bb26badd36bd154234f3da2ca15db8b
aemaddin <aemaddin@gmail.com>
2021-01-13 18:26:04 +0200

    add ability to fitch invoices from billable trait

--- src/Concerns/ManagesInvoices.php
+++ src/Concerns/ManagesInvoices.php
@@ -4,7 +4,13 @@
 namespace Asciisd\Cashier\Concerns;
 
 
+use Asciisd\Cashier\Cashier;
+use Tap\Invoice;
+
 trait ManagesInvoices
 {
-
+    public static function tapInvoices()
+    {
+        Invoice::all(['limit' => 25], Cashier::tapOptions());
+    }
 }
