0e95b03a4dda41c7614c8c56dcef2f845d0ce372
aemaddin <aemaddin@gmail.com>
2023-11-02 00:16:48 +0200

    - [X] Receipt dos not require auth to show it

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['web']], function () {
     Route::get('receipt', 'ReceiptController@show')
-        ->middleware('auth')
         ->name('receipt');
 });
 Route::post('webhook', 'WebhookController@handleWebhook')->name('webhook');
